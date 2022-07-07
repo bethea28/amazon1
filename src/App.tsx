9ee5f9cafe734024ad04345e8ecc63a328c5a74a
Christina Whethers <cwhether@amazon.com>
2022-07-07 11:07:26 -0400

    Reformat page, change local theme into a global theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import InterestSelection from './Components/Signup/InterestSelection';
+import { Box, ThemeProvider } from '@mui/material';
+import * as global from "./Resources/GlobalTheme";
 
 function App() {
+
+  const theme = global.theme;
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/interests" element={<InterestSelection />}/>
-      </Routes>
-    </div>
+    <Box className="App" height={"100vh"} display={"flex"} flexDirection={"column"}>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/interests" element={<InterestSelection />} />
+        </Routes>
+      </ThemeProvider>
+    </Box>
   );
 }
 
--- src/Components/Signup/InterestSelection.tsx
+++ src/Components/Signup/InterestSelection.tsx
@@ -1,6 +1,7 @@
-import { ToggleButton, Container, Grid, Typography, ToggleButtonGroup, Button, ThemeProvider, createTheme } from "@mui/material";
+import { ToggleButton, Grid, Typography, ToggleButtonGroup, Button, Box } from "@mui/material";
 import { styled } from "@mui/system";
 import React, { useState } from "react";
+import Header from "../Header";
 
 function InterestSelection() {
 
@@ -8,25 +9,10 @@ function InterestSelection() {
 
     const ChoiceToggleButton = styled(ToggleButton)({
         backgroundColor: "rgba(144, 216, 111, 0.57)",
-        width: "125px",
+        width: "8rem",
         fontWeight: 600,
         borderRadius: "50px!important",
-        margin: "10px"
     })
-
-    const theme = createTheme({
-        palette: {
-        primary: {
-            main: '#90d86f',
-            },
-        secondary: {
-            main: '#f50057',
-            },
-        },
-        typography: {
-            fontFamily: 'Poppins',
-            },
-    });
 
     function handleChoice(event: React.MouseEvent<HTMLElement>, newChoices: string[]) {
         console.log(event);
@@ -35,36 +21,44 @@ function InterestSelection() {
 
 
     return (
-        <Container>
-            <ThemeProvider theme={theme}>
-            <Grid container spacing={0} sx={{alignItems: "center", justifyContent: "end"}}>
-                <Grid item xs={4} sx={{height: "100vh", backgroundColor: "rgba(209, 225, 210, 0.32)"}}>
-                    <Typography sx={{padding: "60% 0"}} variant="h6">Let's Get Started</Typography>
+        <Box flex={1} overflow={"auto"}>
+            <Header />
+            <Grid container spacing={0} alignItems="center" height={"100%"}>
+                <Grid container xs={4} bgcolor="rgba(209, 225, 210, 0.32)" height={"inherit"} justifyContent={"center"} alignItems={"center"}>
+                    <Grid item>
+                        <Typography variant="h4">Let's Get Started</Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={8} sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <Typography variant="h6">Select what best describes your interests. (Select all that apply)</Typography>
-                    <Container sx={{marginTop: "10%"}}>
-                        <ToggleButtonGroup value={choices} onChange={handleChoice} sx={{flexWrap: "wrap", justifyContent: "center"}}>
-                            <ChoiceToggleButton value="music">Music</ChoiceToggleButton>
-                            <ChoiceToggleButton value="art">Art</ChoiceToggleButton>
-                            <ChoiceToggleButton value="food"> Food</ChoiceToggleButton>
-                            <ChoiceToggleButton value="cars">Cars</ChoiceToggleButton>
-                            <ChoiceToggleButton value="wildlife">Wildlife</ChoiceToggleButton>
-                            <ChoiceToggleButton value="pets">Pets</ChoiceToggleButton>
-                            <ChoiceToggleButton value="technology">Technology</ChoiceToggleButton>
-                            <ChoiceToggleButton value="literature">Literature</ChoiceToggleButton>
-                            <ChoiceToggleButton value="healthcare">Healthcare</ChoiceToggleButton>
-                            <ChoiceToggleButton value="finance">Finance</ChoiceToggleButton>
-                            <ChoiceToggleButton value="sports">Sports</ChoiceToggleButton>
-                            <ChoiceToggleButton value="politics">Politics</ChoiceToggleButton>
-                            <ChoiceToggleButton value="entertainment">Entertainment</ChoiceToggleButton>
-                        </ToggleButtonGroup>
-                    </Container>
-                <Button variant="contained" disabled={choices.length < 2} sx={{alignSelf: "flex-end", width: 125, borderRadius: 50}}>Next</Button>
+                <Grid container height={"100%"} xs={8} alignContent={"space-around"} justifyContent={"center"}>
+                    <Grid item>
+                        <Box>
+                            <Typography variant="h5">Select what best describes your interests</Typography>
+                            <Typography variant="h5">(Select all that apply)</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        <Box>
+                            <ToggleButtonGroup size="large" value={choices} onChange={handleChoice} sx={{ flexWrap: "wrap", justifyContent: "center" }}>
+                                <ChoiceToggleButton value="music">Music</ChoiceToggleButton>
+                                <ChoiceToggleButton value="art">Art</ChoiceToggleButton>
+                                <ChoiceToggleButton value="food"> Food</ChoiceToggleButton>
+                                <ChoiceToggleButton value="cars">Cars</ChoiceToggleButton>
+                                <ChoiceToggleButton value="wildlife">Wildlife</ChoiceToggleButton>
+                                <ChoiceToggleButton value="pets">Pets</ChoiceToggleButton>
+                                <ChoiceToggleButton value="technology">Technology</ChoiceToggleButton>
+                                <ChoiceToggleButton value="literature">Literature</ChoiceToggleButton>
+                                <ChoiceToggleButton value="healthcare">Healthcare</ChoiceToggleButton>
+                                <ChoiceToggleButton value="finance">Finance</ChoiceToggleButton>
+                                <ChoiceToggleButton value="sports">Sports</ChoiceToggleButton>
+                                <ChoiceToggleButton value="politics">Politics</ChoiceToggleButton>
+                                <ChoiceToggleButton value="entertainment">Entertainment</ChoiceToggleButton>
+                            </ToggleButtonGroup>
+                        </Box>
+                    </Grid>
+                    <Button size="large" variant="contained" disabled={choices.length < 2} fullWidth>Next</Button>
                 </Grid>
             </Grid>
-            </ThemeProvider>
-        </Container>
+        </Box>
     )
 }
 
