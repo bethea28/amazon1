8e0ddb7499e3ea908315c8e76ce30ce0d2e97c49
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-07 13:32:35 -0400

    Updated Profile Page with icons and buttons

--- src/Components/Navbar/Profile.tsx
+++ src/Components/Navbar/Profile.tsx
@@ -1,101 +1,169 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
-import { Typography } from '@mui/material';
-import { Container } from '@mui/material';
-import { TextField } from '@mui/material';
-import { Avatar } from '@mui/material';
-import { Grid } from '@mui/material';
-import { Button, Paper } from '@mui/material';
+import { Grid, Paper, Button, TextField} from '@mui/material';
 import './Profile.css';
+import { Dispatch, SetStateAction } from 'react';
+import { Component } from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Typography } from '@mui/material';
+
 
 export default function Profile() {
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        //value: React.Dispatch<React.SetStateAction<string>>
+    };
+
     return (
-        <Box sx={{flexDirection: 'column', alignItems: 'flex-start', height: 300}}>
-            <Box sx={{ 
-                m: 1,
-                display: 'flex', 
-                border: '1px solid',
-                height: '50%',
-                backgroundColor: 'primary.dark'}}>
-            <h1>Header Picture</h1>
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', height: 900 }}>
+      <Box
+        sx={{
+          width: 1,
+          height: 1 / 4,
+          my: 3,
+          p: 1,
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+          color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+          border: '1px solid',
+          borderColor: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+          borderRadius: '5px',
+          fontSize: '0.875rem',
+          fontWeight: '700',
+          textAlign: 'center',
+        }}
+      >
+        Picture
+      </Box>
+      <Box
+        sx={{
+          width: 2 / 3,
+          height: 1,
+          mx: "auto",
+          pl: 3,
+          py: 1,
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+          color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+          border: '1px solid',
+          borderColor: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+          borderRadius: 2,
+          fontSize: '0.875rem',
+          fontWeight: '700',
+          textAlign: 'left',
+        }}
+      >
+        <Box sx={{ fontWeight: 'bold'}}>My Profile</Box>
+
+        <Paper elevation={3} sx={{ flexDirection: 'column',
+          my: 2, mr: 3,
+          height: 1 / 3,
+          borderRadius: '5px',
+          display: 'flex', 
+          backgroundColor: '#EAEAEA'}}>    
+            <Box sx={{
+              height: 1 / 4,
+              width: '100%',
+              borderRadius: '5px',
+              display: 'flex', 
+              backgroundColor: '#335436'}}>
+              <Grid item container direction="row" justifyContent="space-between">
+              <Box sx={{ml: 2, color: '#FFFFFF', typography: 'subtitle2' }}>Personal Information</Box>
+              <Grid item container justifyContent="flex-end" alignItems="flex-end">
+              <Box sx={{}}> <Button variant="contained" onClick={handleClick} endIcon={<KeyboardArrowUpIcon />} ></Button>
             </Box>
-            <Paper elevation={1}
-            sx={{ flexDirection: 'column',
-                m:8,
-                p: 1,
-                height: 600,
-                borderRadius: '10px',
-                display: 'flex', 
-                border: '1px solid',
-                backgroundColor: '#EAEAEA'}}>
-                <Typography variant="subtitle1">My profile</Typography>
-                <Box sx={{ flexDirection: 'column',
-                    m:3,
-                    height: '33%',
-                    width: '90%',
-                    borderRadius: '5px',
-                    display: 'flex', 
-                    border: '1px solid',
-                    backgroundColor: '#EAEAEA'}}>
-                    <Box sx={{
-                        height: '33%',
-                        width: '100%',
-                        borderRadius: '5px',
-                        display: 'flex', 
-                        backgroundColor: '#335436'}}>
-                        <Typography variant="subtitle1">Personal information</Typography>
-                    </Box>
-                    <Grid container direction={"column"} spacing={2}>
-                        <Grid item>
+            </Grid>
+            </Grid>
+            </Box>
+            <Box sx={{ml:2, m: 1, height: 3 / 4}}>
+              <Grid container direction={"column"} component="form" spacing={1}>
+                <Grid item>
                             <TextField id="outlined-basic" label="Name" variant="outlined" size="small"/>
                         </Grid>
                         <Grid item>
                             <TextField id="outlined-basic" label="Username" variant="outlined" size="small"/>
                         </Grid>
                         <Grid item>
-                            <TextField id="outlined-basic" label="Outlined" variant="outlined" size="small"/>
+                            <TextField id="outlined-basic" label="Email" variant="outlined" size="small"/>
                         </Grid>
+                        
                     </Grid>
-                </Box>
-                <Box sx={{ flexDirection: 'column',
-                    m:3,
-                    p: 1,
-                    height: '33%',
-                    width: '90%',
-                    borderRadius: '10px',
-                    display: 'flex', 
-                    border: '2px solid',
-                    backgroundColor: '#EAEAEA'}}>  
-                    <Box sx={{
-                        height: '33%',
-                        width: '100%',
-                        borderRadius: '5px',
-                        display: 'flex', 
-                        backgroundColor: '#335436'}}>
-                        <Typography variant="subtitle1">Bio</Typography>
                     </Box>
-                    <TextField id="outlined-basic" label="Bio" variant="outlined"/>
-                </Box>
-                <Box sx={{ 
-                    m:3,
-                    p: 1,
-                    height: '33%',
-                    width: '90%',
-                    borderRadius: '10px',
-                    display: 'flex', 
-                    border: '2px solid',
-                    backgroundColor: '#EAEAEA'}}>
-                    <Box sx={{
-                        height: '33%',
-                        width: '100%',
-                        borderRadius: '5px',
-                        display: 'flex', 
-                        backgroundColor: '#335436'}}>
-                        <Typography variant="subtitle1">Interest</Typography>
+        </Paper>
+
+        <Paper elevation={3} sx={{ flexDirection: 'column',
+          my: 2, mr: 3,
+          height: 1 / 3,
+          borderRadius: '5px',
+          display: 'flex', 
+          backgroundColor: '#EAEAEA'}}>    
+            <Box sx={{
+              height: 1 / 4,
+              width: '100%',
+              borderRadius: '5px',
+              display: 'flex', 
+              backgroundColor: '#335436'}}>   
+              
+              <Grid item container direction="row" justifyContent="space-between">
+              <Box sx={{ml: 2, color: '#FFFFFF', typography: 'subtitle2' }}>Bio</Box>
+              <Grid item container justifyContent="flex-end" alignItems="flex-end">
+              <Box sx={{}}> <Button variant="contained" onClick={handleClick} endIcon={<KeyboardArrowUpIcon />} ></Button>
+            </Box>
+             
+            </Grid>
+            <Box sx={{ml:2, m: 1, height: 3 / 4}}>
+              <Grid container direction={"column"} component="form" spacing={1}>
+                <Grid item>
+                <TextField
+          id="outlined-multiline"
+          size="medium"
+          label="Bio"
+          multiline
+          rows={5}
+        />
+                        </Grid>
+                        
+                     
+                        
+                    </Grid>
                     </Box>
-                </Box>
-                <Button className="Button" variant="contained">Save Profile</Button>
-            </Paper>
-        </Box>  
-    )
+            
+            </Grid>
+            
+            </Box>
+            
+
+        </Paper>
+
+        <Box sx={{
+              mr: 3,
+              height: 1 / 11,
+              borderRadius: '5px',
+              display: 'flex', 
+              backgroundColor: '#335436'}}>
+              <Grid item container direction="row" justifyContent="space-between">
+              <Box sx={{ml: 2, color: '#FFFFFF', typography: 'subtitle2' }}>Interest</Box>
+              <Grid item container justifyContent="flex-end" alignItems="flex-end">
+              <Box sx={{}}> <Button variant="contained" onClick={handleClick} endIcon={<KeyboardArrowDownIcon />} ></Button>
+            </Box>
+            </Grid>
+            </Grid>
+            </Box>
+
+        {/* { <Paper elevation={3} sx={{ flexDirection: 'column',
+          mr: 3,
+          height: 1 / 4,
+          //borderRadius: '5px',
+          display: 'flex', 
+          backgroundColor: '#EAEAEA'}}>    
+          </Paper> } */}
+            <Grid item container direction="row" justifyContent="flex-end" alignItems="flex-end">
+            <Box sx={{my: 6}}> <Button variant="contained" onClick={handleClick}>Save Profile</Button></Box></Grid>
+      </Box>
+    </Box>
+  );
 }
