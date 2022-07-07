c3fe80f759763d3e7ea9fe5e3029ea98053d4e13
Christina Whethers <cwhether@amazon.com>
2022-07-07 11:06:14 -0400

    Add a header for signup

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,19 @@
-import { Container } from "@mui/system";
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 import React from "react";
 
 function Header() {
     return (
-        <Container>
-            Jumpstarter
-        </Container>
+        <Box>
+            {/* <Typography>Jumpstarter</Typography> */}
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h5" sx={{flexGrow: 1, textAlign: "left", fontWeight: 600}}>Jumpstarter</Typography>
+                        <Button color="secondary">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </Box>
     )
 }
 
