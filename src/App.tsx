97374f9c12b050b125e934b52894beb0e9cfdb8f
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-12 10:44:03 -0400

    Appbar upon login

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,6 @@ import Navbar from "./Components/Navbar/Navbar";
 export default function App() {
   return (
     <div>
-      
-      {/* <h1>Bookkeeper</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
-      </nav> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import App from './App';
 import Profile from './Components/Profile/Profile'
 import Navbar from './Components/Navbar/Navbar';
+import Appbar from './Components/Navbar/Appbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar/>
+    <Appbar/>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="profile" element={<Profile />} />
