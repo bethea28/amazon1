463fb540f996f0d17d8047f9b690b1bebf7717d5
Christina Whethers <cwhether@amazon.com>
2022-07-12 11:09:13 -0400

    Implement search bar

--- src/Routes/Dashboard/Components/SearchBar.tsx
+++ src/Routes/Dashboard/Components/SearchBar.tsx
@@ -1,10 +1,29 @@
-import { Box, TextField } from "@mui/material";
-import React from "react";
+import { Box, TextField, styled, Input, FormControl } from "@mui/material";
+import React, { useState } from "react";
 
 function SearchBar() {
+
+    const [search, setSearch] = useState("");
+
+    const SearchField = styled(Input)({
+        backgroundColor: "#E9E9E9",
+        marginTop: "1em",
+        padding: "0.5em 2em",
+        border: "none",
+        borderRadius: "20px",
+        color: "#7B7D8C",
+    })
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        // Needs logic from project service for project search
+    }
+
     return (
         <Box>
-            <TextField margin="normal" variant="filled" placeholder="Find a project or category" />
+            <form onSubmit={handleSubmit}>
+                <SearchField fullWidth autoFocus value={search} onChange={(event) => setSearch(event.target.value)} type="search" disableUnderline={true} placeholder="Find a project or category" />
+            </form>
         </Box>
     )
 }
