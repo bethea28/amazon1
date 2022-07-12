e4fdbbc1563e84a29c67f796806ac463696ea1af
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-12 19:17:17 -0400

    small edits

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
-import Profile from './Profile'
+//import Profile from './Profile'
 
 export default function AccountMenu() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
