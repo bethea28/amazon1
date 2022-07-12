9216838c55172c6d75362c8b64a3f0b9abe01bee
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-12 14:49:12 -0400

    fixing pull request comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <></>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import App from './App';
-import Profile from './Components/Profile/Profile'
-import Navbar from './Components/Navbar/Navbar';
 import Appbar from './Components/Navbar/Appbar';
 
 const root = ReactDOM.createRoot(
@@ -15,7 +13,6 @@ root.render(
     <Appbar/>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>
   </React.StrictMode>
