aa0ab489af04f118ec4b9b1fa741f245454b8eb2
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-12 14:44:19 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ import ReactDOM from 'react-dom/client';
+ import { BrowserRouter, Routes, Route, } from "react-router-dom";
+ import App from './App';
+ import Profile from './Components/Profile/Profile'
+ import Navbar from './Components/Navbar/Navbar';
+ import Appbar from './Components/Navbar/Appbar';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ const root = ReactDOM.createRoot(
+   document.getElementById('root') as HTMLElement
+ );
+ root.render(
+   <React.StrictMode>
+     <BrowserRouter>
+     <Appbar/>
+     <Routes>
+       <Route path="/" element={<App />} />
+       <Route path="profile" element={<Profile />} />
+     </Routes>
+   </BrowserRouter>
+   </React.StrictMode>
+ );
