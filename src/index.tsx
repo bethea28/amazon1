c93f9d7f9bfc6ea9dfe46ac9e2d35fa922c0f249
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-12 21:41:43 -0400

    Editing menu items

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import App from './App';
-import Appbar from './Components/Navbar/Appbar';
 import Home from './Components/Home/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const isAuth = true;
 root.render(
   <React.StrictMode>
     <BrowserRouter>
