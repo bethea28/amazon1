c2e37637706f027c8558f0fa8c307bf1b75e1174
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-15 12:11:46 -0400

    Rebasing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import React from "react";
+import AppbarPrivate from "./Components/Navbar/AppbarPrivate";
+import AppbarPublic from "./Components/Navbar/AppbarPublic";
+
 export default function App() {
+  const userLoggedIn = false
   return (
-    <></>
+    userLoggedIn ? <AppbarPrivate /> : <AppbarPublic />
   );
 }
