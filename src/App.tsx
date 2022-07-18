799421a7d5191b62216fe178fc8964f7637618f3
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-18 12:46:03 -0400

    User can sign-up and sign-in

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from "react";
-import AppbarPrivate from "./Components/Navbar/AppbarPrivate";
-import AppbarPublic from "./Components/Navbar/AppbarPublic";
-import logo from './logo.svg';
-import './App.css';
-import { CognitoUser } from '@aws-amplify/auth';
-import { BrowserRouter } from 'react-router-dom';
-import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import SignIn from './Components/Signup';
-Amplify.configure(awsconfig);
+import { Auth } from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import axios from "axios"
 
-export default function App() {
-  const userLoggedIn = false
-  return (
-    // userLoggedIn ? <AppbarPrivate /> : <AppbarPublic />
-    <div className="App">
-      <header className="App-header">
-      <SignIn authState={'signIn'} loading={false}/>
+const SignIn = () => {
+    
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      // const { user } = await Auth.signUp({
+      //   username:"Cohort11",
+      //   password:"Nada1998!",
+      //   attributes: {
+      //     email:"bri@email.com"
+      //   }
+      // })
+      // console.log(user);
+    
+    const user2 = await Auth.signIn("Cohort11", "Nada1998!");
+    console.log(user2.signInUserSession.jwtToken)
 
-      </header>
-    </div>
-  );
+    Auth.currentSession().then(res=>{
+                let accessToken = res.getAccessToken()
+                let jwt = accessToken.getJwtToken()
+                //You can print them to see the full objects
+                console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
+                console.log(`myJwt: ${jwt}`)
+
+                // const get = async (userId: string) => {
+   
+                //   const res = await axios.get('http://localhost:8081/profile/001', {
+                //     headers: {
+                //           "Access-Control-Allow-Origin": "*",
+                //       //     // "Content-Type": "application/json",
+                //           "Authorization": `Bearer ${jwt}`
+                //         }
+                //   })
+                // }
+
+                // axios.post('http://localhost:8081/profile/001', {
+                //   headers: {
+                //     "Access-Control-Allow-Origin": "*",
+                //     // "Content-Type": "application/json",
+                //     "Authorization": `Bearer ${jwt}`
+                //   }
+                // }) 
+              })
+
+                          
+    }
+  
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, [])
+  
+  return(
+    <>
+    </>
+  )
 }
+export default SignIn
