9bb5b25c4eb496db2af017a0b5b2d3fffa0e5db8
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-18 14:00:52 -0400

    fixing PR request

--- src/Components/Navbar/AppbarPrivate.tsx
+++ src/Components/Navbar/AppbarPrivate.tsx
@@ -1,14 +1,8 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import React from 'react';
+import { AppBar } from '@mui/material';
+import { Box, Toolbar, Menu, Badge } from '@mui/material';
+import { IconButton, Typography, MenuItem } from '@mui/material';
+import { ListItemText, Divider, ListItemIcon } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
@@ -16,11 +10,8 @@ import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import { useNavigate } from "react-router-dom";
-import ListItemIcon from '@mui/material/ListItemIcon';
 import Logout from '@mui/icons-material/Logout';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { ListItemText } from '@mui/material';
-import Divider from '@mui/material/Divider';
 import {Search, SearchIconWrapper, StyledInputBase} from '../Constants'
 
 export default function AppbarPrivate() {
