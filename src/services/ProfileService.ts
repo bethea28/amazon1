150949847b5f1292f1b815a4e81f39878f189591
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-19 10:18:14 -0400

    fixing errors

--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -3,18 +3,23 @@ import axios from "axios"
 class ProfileService{
 
   get = async (userId: string) => {
-   
-      const res = await axios.get('http://localhost:8081/profile/001')
-      return(res.data)
+    const token = "eyyyyy"
+      const res = await axios.get('http://localhost:8080/profile/001', {
+        headers: {
+          "Content-Type":"application/json",
+          "Authorization": `Bearer $(token)`
+        }
+      })
+    return(res.data)
   }
 
-  update = (data: object) => {
-    axios.put('http://localhost:8081/profile/001', data, {
-      headers: {
-        "Content-Type":"application/json"
-      }
-    })
-  }
+  // update = (data: object) => {
+  //   axios.put('http://localhost:8080/profile/001', data, {
+  //     headers: {
+  //       "Content-Type":"application/json"
+  //     }
+  //   })
+  // }
 }
 
 export default new ProfileService;
