cf0dfc98244430f7d1c28b125206c7e0d41820ce
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-20 09:19:28 -0400

    working on signin

--- src/Services/UserProfileService.ts
+++ src/Services/UserProfileService.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { axiosInstance } from "../Resources/Constants"
+import { Auth } from 'aws-amplify';
 
 class UserProfileService{
 
@@ -12,6 +13,22 @@ class UserProfileService{
     const response = await axiosInstance.put(`/profile/${userId}`, data, {
       headers: {
         "Content-Type":"application/json"
+      }
+    })
+  }
+
+  signUp = async (username: string, password: string, email:string) => {
+       const user = await Auth.signUp({
+      username:"Cohort9",
+      password:"Nada1998!",
+      attributes: {
+        email:"c@email.com"
+      }
+    })
+    const response = await axiosInstance.put(`/profile/${userId}`, data, {
+      headers: {
+        "Content-Type":"application/json",
+        "Authorization": `Bearer ${token}`
       }
     })
   }
