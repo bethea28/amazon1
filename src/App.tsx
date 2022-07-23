3bb2d3658788c391b1b618ab291317b9b2484baf
sheiladzm <sheila@sheiladiezmo.com>
2022-07-22 20:02:33 -0700

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import './App.css';
-import Box from '@mui/material/Box';
 import AvatarUploadField from './Components/AvatarUploadField';
-=======
-import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import InterestSelection from './Routes/Signup/InterestSelection';
@@ -11,16 +7,11 @@ import { Box } from '@mui/material';
 import Profile from './Components/Profile/Profile'
 import Home from './Components/Home/Home';
 import Dashboard from './Routes/Home/Dashboard';
->>>>>>> 3d914ed9cb7d4effc871e2073cf3a3cf8a05db88
 
 function App() {
 
   return (
-<<<<<<< HEAD
-      <Box className="App">
-        <AvatarUploadField />
-      </Box>
-=======
+      
     <Box className="App" height={"100vh"} display={"flex"} flexDirection={"column"}>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,8 +19,10 @@ function App() {
         <Route path="/interests" element={<InterestSelection />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Box className="Avatar Field">
+        <AvatarUploadField />
+      </Box>
     </Box>
->>>>>>> 3d914ed9cb7d4effc871e2073cf3a3cf8a05db88
   );
 }
 
