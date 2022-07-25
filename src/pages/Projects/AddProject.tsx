9ce8a053f8f8eb2e1b9c251499efe187bb2ddafd
yilinshensummer <yilinshen1992@gmail.com>
2022-07-24 18:46:12 -0700

    AddProject.tsx

--- src/pages/Projects/AddProject.tsx
+++ src/pages/Projects/AddProject.tsx
@@ -2,23 +2,28 @@ import React,{useState, useEffect} from 'react'
 import { Typography } from '@mui/material';
 import { FormControl, InputLabel, MenuItem, Button, Box,
          Select,Grid,TextField, Input, Paper,makeStyles } from '@material-ui/core';
-import {MuiPickersUtilsProvider,KeyboardDatePicker}from '@material-ui/pickers'
+import {MuiPickersUtilsProvider,KeyboardDatePicker,DatePicker}from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns';
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useForm, Controller, SubmitHandler, DefaultValues } from "react-hook-form";
 import axiosInstance from '../../apiConfig';
 import { Auth } from 'aws-amplify';
 import { padding } from '@mui/system';
-import { Padding } from '@mui/icons-material'
+import { FireplaceRounded, Padding } from '@mui/icons-material'
 import ReactDatePicker from 'react-datepicker';
 import ReactSelect from 'react-select';
 
+// interface ICategories{ 
+//     label: string;
+//     value: string;
+// }
+
 
 interface ProjectFormInput {
-    projectName: String;
-    targetFundingNum: String;
-    targetFundingDate: Date;
-    description: String;
-    categories:  String;
+    projectName: string;
+    targetFundingNum: string;
+    targetFundingDate: Date | null;
+    description: string;
+    categories: string;
 }
 // export type ProjectFormInput = {
 //     Native: string;
@@ -43,40 +48,46 @@ const useStyles = makeStyles (theme =>({
             background: "rgb(166, 223, 139)",
             borderRadius:10,
             padding: '0 30px',
-        }
+            
+        },
+        "& .MuiFormLabel-root": {
+            padding: '0 30px',
+          }
     },
-    label:{
-        textTransform:'none'
-    }
+    // label:{
+    //     textTransform:'none',
+        
+    // }
    
 }))
 export default function AddProject() {
     const classes = useStyles();
+    const [targetDate, setDate] = useState<Date | null>(null);
 
-    const { control,register, handleSubmit} = useForm<ProjectFormInput>();
+    const { reset, control, register, handleSubmit} = useForm<ProjectFormInput>();
     
-    const currencies = [
-        {
-          value: 'Food',
-          label: 'Food',
-        },
-        {
-          value: 'Art',
-          label: 'Art',
-        },
-        {
-          value: 'Car',
-          label: 'Car',
-        },
-        {
-          value: 'Tech',
-          label: 'Tech',
-        },
-        {
-            value: 'Other',
-            label: 'Other',
-          },
-      ];
+    // const categoriesSelect:ICategories[]=[
+    //     {
+    //       value: 'Food',
+    //       label: 'Food',
+    //     },
+    //     {
+    //       value: 'Art',
+    //       label: 'Art',
+    //     },
+    //     {
+    //       value: 'Car',
+    //       label: 'Car',
+    //     },
+    //     {
+    //       value: 'Tech',
+    //       label: 'Tech',
+    //     },
+    //     {
+    //         value: 'Other',
+    //         label: 'Other',
+    //       },
+    //   ];
     const onSubmit = async (data: ProjectFormInput ) => {
         
         console.log("Name",data)
@@ -93,18 +104,17 @@ export default function AddProject() {
     
     return (
         <Paper>
-        <form className={classes.root} onSubmit={handleSubmit(onSubmit)} autoComplete = "off">
+        <form className={classes.root} onSubmit={handleSubmit(onSubmit)}
+        onReset={() => reset()} autoComplete = "off">
         
             <Grid container>
             <Grid item xs = {4}>
             <Controller
-
                 render={({ field }) => <TextField
                 label = "Input name of your project" {...field}/>}
                 name="projectName"
                 control={control}
                 defaultValue=""
-                 
             />
             </Grid>
             <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
@@ -114,13 +124,11 @@ export default function AddProject() {
             <Grid container>
             <Grid item xs = {4}>
             <Controller
-
                 render={({ field }) => <TextField 
                 label = "Input target funding amount "{...field}/>}
                 name="targetFundingNum"
                 control={control}
-                defaultValue=""
-                 
+                defaultValue=""    
             />
             </Grid>
             <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
@@ -129,103 +137,103 @@ export default function AddProject() {
             </Grid>
             <Grid container>
             <Grid item xs = {4}>
-             <FormControl variant = "filled">
-             <TextField
-                select
-                defaultValue=""
-                label="Select categories"
-                name="categories"
-                inputProps={{
-                    inputRef: (ref: { value: any; }) => {
-                      if (!ref) return;
-                      register({
-                        label: "categories",
-                        value: ref.value,
-                      }); 
-                    },
-                  }}
-                >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-             </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="category-select">
+                    Select Category 
+                </InputLabel>
+               
+                <Controller
+                    name="categories"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => {
+                    return <Select 
+                        value={field.value}
+                        onChange={(e) => field.onChange(e)}
+                     >
+                        <MenuItem value="Tech">Tech</MenuItem>
+                        <MenuItem value="Art">Art</MenuItem>
+                        <MenuItem value="Car">Car</MenuItem>
+                        <MenuItem value="Food">Food</MenuItem>
+                        <MenuItem value="Pet">Pet</MenuItem>
+                        <MenuItem value="Other">Other</MenuItem>
+                    </Select>
+                    }}
+                />
+                
+            </FormControl>
             </Grid>
             <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
                 <Typography>Select Categories</Typography>
             </Grid>
             </Grid>
+            
             <Grid container>
             <Grid item xs = {4}>
             <Controller
+              name="targetFundingDate"
+              defaultValue = {null}
+              control={control}
+              render={({ field }) => (
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <KeyboardDatePicker
+                    inputVariant="outlined"
+                    variant="inline"
+                    label="Select Date"
+                    value={field.value}
+                    onChange={(e)=>field.onChange(e)}
+                    format="MM/dd/yyyy"
+                    defaultValue = {null}
+                    autoOk
+                  />
+                </MuiPickersUtilsProvider>
+            )}
+            />
 
+            </Grid>
+            <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
+                <Typography>Target Funding Date</Typography>
+            </Grid>
+            <Grid container>
+            <Grid item xs = {4}>
+            <Controller
                 render={({ field }) => <TextField 
                 label = "Input descriptions of your project"
                 multiline minRows={12} {...field}/>}
                 name="description"
                 control={control}
                 defaultValue=""
-                 
             />
             </Grid>
             <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
                 <Typography>Project Description</Typography>
             </Grid>
-            </Grid>
-            <Grid container>
-            <Grid item xs = {4}>
-            
-            <Controller
-              control={control}
-              name="targetFundingDate"
-              render={({ field: { value, ...fieldProps } }) => {
-                return (
-                    <ReactDatePicker
-                    {...fieldProps}
-                    className="input"
-                    placeholderText="Select date"
-                    selected={value}
-                  />
-                );
-              }}
-            />
-            
-            
-            </Grid>
-            <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
-                <Typography>Project Description</Typography>
             </Grid>
             <Grid container>
             <Grid item xs = {4}>
             <Button variant="contained" 
             color="primary"
-            type="button"
+            type="reset"
             >
-            <Typography variant="button">Submit</Typography>
+                <Typography variant="button">Reset</Typography>
             </Button>
             </Grid>
             <Grid item xs = {2} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
             <Button variant="contained" 
             color="primary"
             type="submit">
-            <Typography variant="button">Submit</Typography>
+                <Typography variant="button">Submit</Typography>
             </Button>
             </Grid>
             </Grid>
             
             </Grid>
-            
-            
-      
+
 
         </form>
         </Paper>
         
     )
 }
-function register(arg0: { name: string; value: any; }) {
-    throw new Error('Function not implemented.');
-}
+
 
