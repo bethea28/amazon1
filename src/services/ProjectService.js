1edbdc4980079e82739040bde005364c35563d53
yilinshensummer <yilinshen1992@gmail.com>
2022-07-25 00:24:22 -0700

    removed ProjectService.js

--- src/services/ProjectService.js
+++
@@ -1,8 +0,0 @@
-
-export const getCategoryCollection = () =>([
-    {id: '1', title: 'Music'},
-    {id: '2', title: 'Food'},
-    {id: '3', title: 'Art'},
-    {id: '4', title: 'Pet'},
-    {id: '5', title: 'Other'}
-])
---
+++ src/services/ProjectService.tsx
@@ -0,0 +1,14 @@
+import axiosInstance from '../apiConfig';
+import { Auth } from 'aws-amplify';
+
+export const postData = async (state:any) => {
+
+    const res = await Auth.currentSession()
+    let jwt = res.getAccessToken().getJwtToken();    
+    return await axiosInstance.post('project', state, {
+        headers: {
+            'Authorization': `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        }
+    }) 
+}
