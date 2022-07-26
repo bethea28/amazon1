ef763086226bd61dd0d2f87781181fc70bf1373c
yilinshensummer <yilinshen1992@gmail.com>
2022-07-25 17:00:52 -0700

    remove .env

--- src/services/ProjectService.tsx
+++ src/services/ProjectService.tsx
@@ -4,7 +4,7 @@ import { Auth } from 'aws-amplify';
 export const postData = async (state:any) => {
     const res = await Auth.currentSession()
     let jwt = res.getAccessToken().getJwtToken();    
-    return await axiosInstance.post('project', state, {
+    return axiosInstance.post('project', state, {
         headers: {
             'Authorization': `Bearer ${jwt}`,
             'Content-Type': 'application/json'
