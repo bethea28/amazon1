6721f3f3c1f2971ab43b124241b572c7db8f0b9a
sheiladzm <sheila@sheiladiezmo.com>
2022-07-26 08:50:46 -0700

    Remove extras and reload

--- src/services/AvatarService.tsx
+++ src/services/AvatarService.tsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+/**
+ * Special case - own config since content types include form-data and binary
+ */
 const http = axios.create({
     baseURL: 'http://localhost:8080/users'
 });
@@ -10,17 +13,13 @@ const http = axios.create({
  * @params filename The image file to fetched by filename
  */
 export async function getAvatar(userId: String, filename: String) { 
-
   try {
-
     const { data } = await http.get(`/getAvatar/${userId}/${filename}`);
     //Add auth token
     return data;
-
   } catch(error) {
     console.log(error);
   }
-
 }
 
 /**
@@ -29,23 +28,16 @@ export async function getAvatar(userId: String, filename: String) {
  * @params file The image file to be saved via form data
  */
 export async function uploadAvatar(userId: String, file: FormData) {
-
   try {
-
     const response = await http.post(`/${userId}/uploadAvatar`, file);
     //Add auth token
-
     if (response.status === 201) {
       alert('Upload successful!');
     }
-
   } catch(error) {
       console.log(error);
       alert('Upload failed! Please try again.')
     }
-
-    window.location.reload();
-
 }
 
 /**
@@ -54,21 +46,14 @@ export async function uploadAvatar(userId: String, file: FormData) {
  * @params filename The image file to delete
  */
 export async function deleteAvatar(userId: String, filename: String) { 
-
   try {
-
     const response = await http.delete(`/deleteAvatar/${userId}/${filename}`);
     //Add auth token
-    
     if (response.status === 204) {
       alert('Delete successful!');
     }
-    
   } catch(error) {
       console.log(error);
       alert('Failed to delete! Please try again.')
     }
-
-    window.location.reload();
-
 }
