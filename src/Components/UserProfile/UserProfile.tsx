7cf1f38b4a69ad4f60451fcdb3ed36ce03ae3095
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-07-26 20:50:01 -0400

    addressing merge comments

--- src/Components/UserProfile/UserProfile.tsx
+++ src/Components/UserProfile/UserProfile.tsx
@@ -14,13 +14,11 @@ import { AuthContext} from '../../Context/AuthProvider';
 import { ProfileSectionStyle } from './UserProfileStyle';
 
 export default function UserProfile() {
-    
+
   const initialValues = {name: '', email: '', bio: ''}
   const [userProfile, setUserProfile] = useState<UserData>(initialValues)
   const { register, handleSubmit, reset } = useForm<UserData>();
   const [userLoggedIn, setUserLoggedIn] = useState<boolean>(true);
-  
-  //get user context
   const { id, token, setAuthData } = useContext(AuthContext)
 
   useEffect(() => {
@@ -35,113 +33,112 @@ export default function UserProfile() {
 
   const onSubmit = handleSubmit(async (data: UserData)=>{
     const response = await UserProfileService.updateUserProfile(id, token, data)
-
 })
 
   return (
     <StyledEngineProvider injectFirst>
-  <form onSubmit={onSubmit}>  
-      <Box sx={{ display: 'flex', flexDirection: 'column', height: 1000}}>
-        {userLoggedIn ? <AppbarPrivate /> : <AppbarPublic />}
-        <Box sx={{...profileBackgroundImageBox}}>
-          Picture
-        </Box>
-        <Box sx={{...profileDataBox}}>
-          <Box sx={{ fontWeight: 'bold'}}>My Profile</Box>
-          <Paper elevation={3} className="customPaper" >    
-            <Box className="customBox">
-              <ProfileSectionStyle label="Personal Information"></ProfileSectionStyle>
-            </Box>
-            <Box sx={{ml:2, m: 1, height: 3 / 4}}>
-              <Grid container direction={"column"} 
-              spacing={1}>
-                <Grid item>
-                  <TextField type="text"
-                  {...register('name', {
-                    onChange: (event: React.ChangeEvent<HTMLInputElement>): 
-                    void => {setUserProfile((prevState) => ({ 
-                      name: event.target.value,
-                      email: prevState.email,
-                      bio: prevState.bio
-                   }))
-                  }
-                })}
-                  fullWidth
-                  id="outlined-basic" 
-                  size="small"
-                  label="Name"
-                  InputLabelProps={{shrink: true}}
-                  value={userProfile!.name}
-                  />
-                </Grid>
-                <Grid item>
-                    <TextField {...register('email')}
-                    onChange={event => {
-                      setUserProfile(prevState => {
-                        return {...prevState, ['email']: event.target.value};
-                      });
-                    }}
-                    fullWidth id="outlined-basic" label="email" 
-                    InputLabelProps={{shrink: true}} 
-                    value={userProfile!.email} 
-                    variant="outlined" size="small" 
-                    />
-                </Grid>
-                <Grid item>
-                  <Avatar />
-                </Grid>
-              </Grid>
-            </Box>
-          </Paper>
-          <Paper elevation={3} className="customPaper">    
-            <Box className="customBox">  
-              <ProfileSectionStyle label="Bio"></ProfileSectionStyle>
-            </Box>
-            <Box sx={{m:1, height: 3 / 4}}>
-              <Grid container direction={"column"} 
-              spacing={1}>
-                <Grid item>
-                  <TextField {...register('bio')}
-                  fullWidth
-                    id="outlined-multiline"
-                    size="medium"
-                    label="Bio"
-                    value={userProfile!.bio}
-                    onChange={event => {
-                      setUserProfile(prevState => {
-                        return {...prevState, ['bio']: event.target.value};
-                      });
-                  }}
-                  multiline={false}
-                    InputLabelProps={{shrink: true}}
-                  />
-                </Grid>     
-              </Grid>
-            </Box>
-          </Paper>
-          <Paper elevation={3} className="customPaper">    
-            <Box className="customBox">  
-              <ProfileSectionStyle label="Interest"></ProfileSectionStyle>
-            </Box>
-            <Box sx={{m:1, height: 3 / 4}}>
-              <Grid container direction={"column"} 
-              spacing={1}>
-                <Grid item>
-                </Grid>     
-              </Grid>
-            </Box>
-          </Paper>        
-          <Grid item container direction="row" justifyContent="flex-end" alignItems="flex-end">
-            <Grid item>
-              <Box sx={{my: 2, mr:3}}> 
-                <Button sx={{backgroundColor:"#A6BBA7", color:"#000000", borderRadius:50}} variant="contained" 
-                onClick={onSubmit}
-                >Save Profile</Button>
+      <form onSubmit={onSubmit}>  
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: 1000}}>
+          {userLoggedIn ? <AppbarPrivate /> : <AppbarPublic />}
+          <Box sx={{...profileBackgroundImageBox}}>
+            Picture
+          </Box>
+          <Box sx={{...profileDataBox}}>
+            <Box sx={{ fontWeight: 'bold'}}>My Profile</Box>
+            <Paper elevation={3} className="customPaper" >    
+              <Box className="customBox">
+                <ProfileSectionStyle label="Personal Information"></ProfileSectionStyle>
               </Box>
+              <Box sx={{ml:2, m: 1, height: 3 / 4}}>
+                <Grid container direction={"column"} 
+                spacing={1}>
+                  <Grid item>
+                    <TextField type="text"
+                    {...register('name', {
+                      onChange: (event: React.ChangeEvent<HTMLInputElement>): 
+                      void => {setUserProfile((prevState) => ({ 
+                        name: event.target.value,
+                        email: prevState.email,
+                        bio: prevState.bio
+                    }))
+                    }
+                  })}
+                    fullWidth
+                    id="outlined-basic" 
+                    size="small"
+                    label="Name"
+                    InputLabelProps={{shrink: true}}
+                    value={userProfile!.name}
+                    />
+                  </Grid>
+                  <Grid item>
+                      <TextField {...register('email')}
+                      onChange={event => {
+                        setUserProfile(prevState => {
+                          return {...prevState, ['email']: event.target.value};
+                        });
+                      }}
+                      fullWidth id="outlined-basic" label="email" 
+                      InputLabelProps={{shrink: true}} 
+                      value={userProfile!.email} 
+                      variant="outlined" size="small" 
+                      />
+                  </Grid>
+                  <Grid item>
+                    <Avatar />
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+            <Paper elevation={3} className="customPaper">    
+              <Box className="customBox">  
+                <ProfileSectionStyle label="Bio"></ProfileSectionStyle>
+              </Box>
+              <Box sx={{m:1, height: 3 / 4}}>
+                <Grid container direction={"column"} 
+                spacing={1}>
+                  <Grid item>
+                    <TextField {...register('bio')}
+                    fullWidth
+                      id="outlined-multiline"
+                      size="medium"
+                      label="Bio"
+                      value={userProfile!.bio}
+                      onChange={event => {
+                        setUserProfile(prevState => {
+                          return {...prevState, ['bio']: event.target.value};
+                        });
+                    }}
+                    multiline={false}
+                      InputLabelProps={{shrink: true}}
+                    />
+                  </Grid>     
+                </Grid>
+              </Box>
+            </Paper>
+            <Paper elevation={3} className="customPaper">    
+              <Box className="customBox">  
+                <ProfileSectionStyle label="Interest"></ProfileSectionStyle>
+              </Box>
+              <Box sx={{m:1, height: 3 / 4}}>
+                <Grid container direction={"column"} 
+                spacing={1}>
+                  <Grid item>
+                  </Grid>     
+                </Grid>
+              </Box>
+            </Paper>        
+            <Grid item container direction="row" justifyContent="flex-end" alignItems="flex-end">
+              <Grid item>
+                <Box sx={{my: 2, mr:3}}> 
+                  <Button sx={{backgroundColor:"#A6BBA7", color:"#000000", borderRadius:50}} variant="contained" 
+                  onClick={onSubmit}
+                  >Save Profile</Button>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
       </form>   
     </StyledEngineProvider>
   );
