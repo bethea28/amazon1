ea11b664fbaa4f2779c0457f24c1fb9d87a81544
sheiladzm <sheila@sheiladiezmo.com>
2022-07-29 14:44:56 -0700

    Add separate details component

---
+++ src/Routes/ProjectDetails/Components/ProjectDetailsList.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import { GetProjectsResponse } from "../../../Resources/Constants";
+
+export default function ProjectDetailsList(props: GetProjectsResponse) {
+
+    return (
+        <Box className="Project-details">
+            
+            {props.projects && props.projects.map((project, idx) => {
+                return (
+                    <Box key={idx}>
+                        <Box>
+                            <h2>Title: {project.projectName}</h2>
+                            <h2>Categories: {project.categories}</h2>
+                            <h2>Description: {project.description}</h2>
+                        </Box>
+                    </Box>
+                )
+            })}
+            
+        </Box>
+    )
+
+}
