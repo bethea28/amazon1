7b8d30e1905d5fb92c04ee819339cba095997c86
sheiladzm <sheila@sheiladiezmo.com>
2022-07-29 19:20:24 -0700

    Add image, title, dates, description, categories

--- src/Routes/ProjectDetails/Components/ProjectDetailsList.tsx
+++ src/Routes/ProjectDetails/Components/ProjectDetailsList.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Chip, Stack, Typography } from "@mui/material";
+import { flexbox } from "@mui/system";
 import { GetProjectsResponse } from "../../../Resources/Constants";
 
 export default function ProjectDetailsList(props: GetProjectsResponse) {
@@ -8,13 +9,48 @@ export default function ProjectDetailsList(props: GetProjectsResponse) {
             
             {props.projects && props.projects.map((project) => {
                 return (
-                    <Box key={project.projectId}>
-                        <Box>
-                            <h2>Title: {project.projectName}</h2>
-                            <h2>Categories: {project.categories}</h2>
-                            <h2>Description: {project.description}</h2>
-                        </Box>
-                    </Box>
+                    <Stack 
+                        key={project.projectId}
+                        direction="column"
+                        justifyContent="space-evenly"
+                        alignItems="center"
+                        spacing={2}
+                        m={5}
+                    >
+                        <Card sx={{ maxWidth: 1000 }}>
+                            <CardMedia
+                                component="img"
+                                height="500"
+                                image="https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg"
+                                alt="Paella dish"
+                            />
+                            {/* Update Image to project photos prop */}
+                            <CardContent>
+                                <Typography variant="h1" m={1}>
+                                {project.projectName}
+                                </Typography>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-around"
+                                    alignItems="center"
+                                    spacing={2}
+                                    >
+                                    <Chip label={project.categories} sx={{backgroundColor: 'rgb(166, 223, 139)' }}/>
+                                    <Typography variant="subtitle1" m={3}>
+                                    <Typography sx={{ fontWeight: 1000 }}>Created:</Typography> {project.createdAt} <Typography sx={{ fontWeight: 1000 }}>Last Updated:</Typography> {project.lastUpdatedAt}
+                                    </Typography>
+                                </Stack>
+                                {/* Insert targetFundingDate and targetFundingNum component here
+                                Insert user data (avatar and name) component */}
+                                <Typography variant="body1" m={3}>
+                                {project.description}
+                                </Typography>
+                            </CardContent>
+                            {/* Insert like component
+                            Insert comments component */}
+                        </Card>
+
+                    </Stack>
                 )
             })}
             
--- src/Routes/ProjectDetails/ProjectDetails.tsx
+++ src/Routes/ProjectDetails/ProjectDetails.tsx
@@ -1,9 +1,16 @@
-import { Box, TextField, Avatar, Button } from '@mui/material';
+import { Box, TextField, Avatar, Button, ThemeProvider, Container } from '@mui/material';
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import { Project, GetProjectsResponse } from '../../Resources/Constants';
 import { getProjectDetails } from '../../Services/ProjectService';
 import ProjectDetailsList from './Components/ProjectDetailsList';
+import { theme } from "../../Resources/GlobalTheme";
+import NavigationBar from '../Dashboard/Components/NavigationBar';
+import SearchBar from '../Dashboard/Components/SearchBar';
+import Header from '../../Components/Header';
+import AppbarPrivate from '../../Components/Navbar/AppbarPrivate';
+import AppbarPublic from '../../Components/Navbar/AppbarPublic';
+
 
 
 export default function ProjectDetails() {
@@ -12,20 +19,9 @@ export default function ProjectDetails() {
     //useParams or Context to pass in global projectId variable
     const projectId = "f476a834-cfea-4184-8596-d7bfbce50dfd"
 
-    // //Empty project details to be filled in once page renders
-    // const initialProjectState: Project[] = [{
-    //     projectId: "",
-    //     userId: "",
-    //     projectName: "",
-    //     targetFundingNum: "",
-    //     targetFundingDate: "",
-    //     description: "",
-    //     categories: "",
-    //     createdAt: "",
-    //     lastUpdatedAt: ""
-    // }]
-
     const [currentProject, setCurrentProject] = useState<Project[]>();
+    const [userLoggedIn, setUserLoggedIn] = useState<boolean>(true);
+
 
     //As soon as the page is rendered, getData function will get called
     useEffect(() => {
@@ -43,8 +39,17 @@ export default function ProjectDetails() {
     }, [currentProject]);
 
     return (
-        <Box className="Project-details">
-            <ProjectDetailsList projects={currentProject!} />
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box>
+            {userLoggedIn ? <AppbarPrivate /> : <AppbarPublic />}
+                <Container>
+                    <SearchBar />
+                    <NavigationBar />
+                    <Box className="Project-details">
+                        <ProjectDetailsList projects={currentProject!} />
+                    </Box>
+                </Container>
+            </Box>
+        </ThemeProvider>
     )
 }
