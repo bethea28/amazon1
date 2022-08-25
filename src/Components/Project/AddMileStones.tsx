b559353019044141efdc160f59e16c218cb3c43e
yilinshensummer <yilinshen1992@gmail.com>
2022-08-25 09:08:04 -0700

    update

--- src/Components/Project/AddMileStones.tsx
+++ src/Components/Project/AddMileStones.tsx
@@ -1,7 +1,12 @@
 import React,{useState, useEffect} from 'react';
 import { Milestone } from '../../Resources/Constants'
-import { makeStyles } from '@material-ui/core';
-
+import { makeStyles, ExpansionPanel, ExpansionPanelSummary,
+     Typography, ExpansionPanelDetails, Paper, Grid } from '@material-ui/core';
+//import ExpandMoreIcon from "@material-ui/icons";
+import { Button, Stack, TextField } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DatePicker } from 'material-ui';
 
 const useStyles = makeStyles(theme => ({
     formControl: {
@@ -14,7 +19,7 @@ const useStyles = makeStyles(theme => ({
 }))
 
 
-export default function AddMileStones() {
+export function AddMileStones() {
     const classes = useStyles();
 
     const [milestones, setMilestones] = useState<Milestone[]>([]);
@@ -28,14 +33,54 @@ export default function AddMileStones() {
             amount: 0,
             targetDate: new Date()
         }
-
+        //const newMilestones = [...milestones]
         //milestones.push(milestone1);
         setMilestones([...milestones, milestone1]);
         setSd(sd + 1);
     };
-    console.log("p", milestones);
+    const removeMilestones = () => {
+
+        const newMilestones = [...milestones]
+        newMilestones.pop();
+
+        setMilestones([...newMilestones]);
+        setSd(sd - 1);
+    };
+    console.log("milestone", milestones);
     
     return (
-        <div>AddMileStones</div>
+        <div>
+        <Grid container>
+            {milestones.map((milestone) =>{
+                return <MileStoneFormRow/>
+                //<MilestonePanel sd = {sd} />
+            })}
+        <Button variant="contained" endIcon={<AddIcon/>} onClick={() => addMilestones()}>
+
+        </Button>
+        <Button variant="contained" endIcon={<DeleteIcon/>} onClick={() => removeMilestones()}>
+
+        </Button>
+        </Grid>
+        </div>
     )
 }
+
+  export const MileStoneFormRow = () => {
+    let milestone2: Milestone = {
+        name: "",
+        amount: 0,
+        targetDate: new Date()
+    }
+    return (
+    <Stack>
+        <TextField />
+        <TextField />
+        <DatePicker />
+        <AddIcon />
+        <DeleteIcon />
+    </Stack>
+
+    )
+}
+  
