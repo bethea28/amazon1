7437df8c00a90a974f9f0e8be6268b4d1fe96f33
sheiladzm <sheila@sheiladiezmo.com>
2022-08-27 17:19:48 -0700

    Use map and index to remove repeating code

--- src/Components/Project/Components/UploadGalleryPhotos.tsx
+++ src/Components/Project/Components/UploadGalleryPhotos.tsx
@@ -16,6 +16,7 @@ export default function UploadGalleryPhotos() {
     }
 
     const handleGalleryUpload = (e: React.MouseEvent<HTMLElement>) => {
+
         let bodyFormData = new FormData();
         bodyFormData.append('file', file);
         uploadPhoto(id!, bodyFormData, false).then(
@@ -28,32 +29,9 @@ export default function UploadGalleryPhotos() {
         );
     }
 
-    const handleGalleryDeleteOne = (e: React.MouseEvent<HTMLElement>) => {
-        const filename = photoURLs[1].substring(photoURLs[1].lastIndexOf("/") + 1);
-        deletePhoto(id!, filename).then(
-            (value) => {
-                window.location.reload();
-            },
-            (reason) => {
-                window.location.reload();
-            }
-        );
-    }
-
-    const handleGalleryDeleteTwo = (e: React.MouseEvent<HTMLElement>) => {
-        const filename = photoURLs[2].substring(photoURLs[2].lastIndexOf("/") + 1);
-        deletePhoto(id!, filename).then(
-            (value) => {
-                window.location.reload();
-            },
-            (reason) => {
-                window.location.reload();
-            }
-        );
-    }
-
-    const handleGalleryDeleteThree = (e: React.MouseEvent<HTMLElement>) => {
-        const filename = photoURLs[3].substring(photoURLs[3].lastIndexOf("/") + 1);
+    const handleGalleryDelete = (e: React.MouseEvent<HTMLElement>, index: number) => {
+        console.log("handle", e, index);
+        const filename = photoURLs[index].substring(photoURLs[index].lastIndexOf("/") + 1);
         deletePhoto(id!, filename).then(
             (value) => {
                 window.location.reload();
@@ -66,14 +44,14 @@ export default function UploadGalleryPhotos() {
 
     const imageOnErrorHandler = (
         event: React.SyntheticEvent<HTMLImageElement, Event>
-      ) => {
+    ) => {
         event.currentTarget.src = "https://dominionmartialarts.com/wp-content/uploads/2017/04/default-image-620x600.jpg";
         event.currentTarget.className = "error";
-      };
+    };
 
     return (
         <Grid container spacing={2} sx={{ justifyContent: 'center', boxShadow: 3, margin: 2, padding: 3 }}>
-        <Grid xs={12}>
+            <Grid xs={12}>
                 <Box sx={{ textAlign: 'left', margin: 2 }}><Typography variant="h5" m={1}>Upload up to 3 photos for your gallery: </Typography></Box>
             </Grid>
             <Grid xs={12}>
@@ -92,83 +70,42 @@ export default function UploadGalleryPhotos() {
                         variant="outlined"
                         onChange={handleChange}
                     />
-                    <Button 
+                    <Button
                         className="uploadPhoto"
-                        sx={{ margin: 1, backgroundColor:"#A6BBA7", color:"#000000", mt:1, height: 50 }}
-                        variant="contained" 
+                        sx={{ margin: 1, backgroundColor: "#A6BBA7", color: "#000000", mt: 1, height: 50 }}
+                        variant="contained"
                         component="span"
                         onClick={handleGalleryUpload}
                     >Upload</Button>
                 </Box>
             </Grid>
-            
-            
-            <Grid xs={4}>
-                <Box sx={{ display: 'flex', margin: 2, justifyContent: 'center' }}>
-                <Card sx={{ maxWidth: 300 }}>
-                    <CardMedia
-                        component="img"
-                        height="200"
-                        width="200"
-                        image={photoURLs[1] || noPhoto}
-                        onError={imageOnErrorHandler}
-                        alt="Project photo"
-                    />
-                    <CardContent>
-                        <Button className="deletePhoto"
-                            sx={{ margin: 1, backgroundColor:"#FFF", mt:1, height: 25 }}
-                            variant="contained" 
-                            component="span"
-                            onClick={handleGalleryDeleteOne}
-                        >Delete</Button>
-                    </CardContent>
-                </Card>
-                </Box>
-            </Grid>
-            <Grid xs={4}>
-                <Box sx={{ display: 'flex', margin: 2, justifyContent: 'center' }}>
-                <Card sx={{ maxWidth: 300 }}>
-                    <CardMedia
-                        component="img"
-                        height="200"
-                        width="200"
-                        image={photoURLs[2] || noPhoto}
-                        onError={imageOnErrorHandler}
-                        alt="Project photo"
-                    />
-                    <CardContent>
-                        <Button className="deletePhoto"
-                            sx={{ margin: 1, backgroundColor:"#FFF", mt:1, height: 25 }}
-                            variant="contained" 
-                            component="span"
-                            onClick={handleGalleryDeleteTwo}
-                        >Delete</Button>
-                    </CardContent>
-                </Card>
-                </Box>
-            </Grid>
-            <Grid xs={4}>
-                <Box sx={{ display: 'flex', margin: 2, justifyContent: 'center' }}>
-                <Card sx={{ maxWidth: 300 }}>
-                    <CardMedia
-                        component="img"
-                        height="200"
-                        width="200"
-                        image={photoURLs[3] || noPhoto}
-                        onError={imageOnErrorHandler}
-                        alt="Project photo"
-                    />
-                    <CardContent>
-                        <Button className="deletePhoto"
-                            sx={{ margin: 1, backgroundColor:"#FFF", mt:1, height: 25 }}
-                            variant="contained" 
-                            component="span"
-                            onClick={handleGalleryDeleteThree}
-                        >Delete</Button>
-                    </CardContent>
-                </Card>
-                </Box>
-            </Grid>
+
+            {photoURLs.map((a, index) => {
+                return (
+                    <Grid xs={4}>
+                        <Box sx={{ display: 'flex', margin: 2, justifyContent: 'center' }}>
+                            <Card sx={{ maxWidth: 300 }}>
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    width="200"
+                                    image={a || noPhoto}
+                                    onError={imageOnErrorHandler}
+                                    alt="Project photo"
+                                />
+                                <CardContent>
+                                    <Button className="deletePhoto"
+                                        sx={{ margin: 1, backgroundColor: "#FFF", mt: 1, height: 25 }}
+                                        variant="contained"
+                                        component="span"
+                                        onClick={(e: any) => handleGalleryDelete(e, index)}
+                                    >Delete</Button>
+                                </CardContent>
+                            </Card>
+                        </Box>
+                    </Grid>
+                )
+            })}
         </Grid>
     )
 }
