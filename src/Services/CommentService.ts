dedf13e36e473a6be0f969a16bcddfe15e54de7b
Christina Whethers <cwhether@amazon.com>
2022-08-29 18:03:33 -0400

    Add JSdocs

--- src/Services/CommentService.ts
+++ src/Services/CommentService.ts
@@ -1,6 +1,12 @@
 import { axiosInstance, CommentData } from "../Resources/Constants";
 import { Auth } from "aws-amplify";
 
+
+/**
+ * Gets a list of comments by project ID
+ * @param projectId A project id to be searched
+ * @returns A promise containing a list of comments relating to the current project
+ */
 export async function getProjectComments(projectId: string) {
     try {
         const { data } = await axiosInstance.get<CommentData[]>("/comments", {
@@ -16,7 +22,12 @@ export async function getProjectComments(projectId: string) {
         console.log(error);
     }
 }
-
+/**
+ * Adds a new comment
+ * @param comment A comment to be added
+ * @param jwt A JWT token authorizing a user to add a comment
+ * @returns A promise containing the newly created CommentData
+ */
 export async function postComment(comment: CommentData, jwt: string) {
     try {
         const { data } = await axiosInstance.post<CommentData>("/comments", comment, {
