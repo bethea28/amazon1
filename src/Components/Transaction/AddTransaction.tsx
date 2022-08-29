ccc8bafb5b6c771ec5ec4c12f358df5aadb96884
yilinshensummer <yilinshen1992@gmail.com>
2022-08-29 12:08:52 -0700

    update the transaction list without touch the backend

--- src/Components/Transaction/AddTransaction.tsx
+++ src/Components/Transaction/AddTransaction.tsx
@@ -1,113 +1,145 @@
-import React, {useContext} from 'react'
-import { Transaction } from '../../Resources/Constants';
-import { Typography, FormHelperText, Stack } from '@mui/material';
-import { FormControl, InputLabel, MenuItem, Button,
-  Select, Grid, TextField, Paper, makeStyles } from '@material-ui/core';
-import { useForm, Controller} from "react-hook-form";
-import {postTransacData} from '../../Services/AddTransactionService';
-import{ProjectIdContext, TransactionContext} from '../Transaction/TransactionTable';
-
-
+import React, { useContext } from "react";
+import { Transaction } from "../../Resources/Constants";
+import { Typography, FormHelperText, Stack } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Button,
+  Select,
+  Grid,
+  TextField,
+  Paper,
+  makeStyles,
+} from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
+import { postTransacData } from "../../Services/AddTransactionService";
+import {
+  ProjectIdContext,
+  TransactionContext,
+} from "../Transaction/TransactionTable";
 
 interface TransactionFormInput {
-    projectId: string;
-    amount: number;
+  projectId: string;
+  amount: number;
 }
-const useStyles = makeStyles (theme =>({
+const useStyles = makeStyles((theme) => ({
   root: {
-      '& .MuiFormControl-root': {
-          width:'80%',
-          margin: theme.spacing(1),
-          background: "rgb(166, 223, 139)",
-          borderRadius:10,
-          padding: '0 30px',
-          
-      },
-      "& .MuiFormLabel-root": {
-          padding: '0 30px',
-      }
-  }
-}))
+    "& .MuiFormControl-root": {
+      width: "80%",
+      margin: theme.spacing(1),
+      background: "rgb(166, 223, 139)",
+      borderRadius: 10,
+      padding: "0 30px",
+    },
+    "& .MuiFormLabel-root": {
+      padding: "0 30px",
+    },
+  },
+}));
 
-export default function AddTransaction() {
-    //let listUpdated = props.listUpdated;
-    //let transactions = props.transactions;
-    
-    const projectIdTrans = useContext<string>(ProjectIdContext);
-    let transactionsList = useContext<Transaction[]>(TransactionContext);
+interface Props {
+  onTransactionCreated: (transaction: Transaction) => void;
+}
 
-    console.log("hi",projectIdTrans);
+export default function AddTransaction({ onTransactionCreated }: Props) {
+  const projectIdTrans = useContext<string>(ProjectIdContext);
+  console.log("hi", projectIdTrans);
 
-    const classes = useStyles();
-    const { reset, control, register, handleSubmit,formState: { errors }} = useForm<TransactionFormInput>();
-    
-    const onSubmit = async (data: TransactionFormInput) => {
-        //const{projectId, amount} = data
+  const classes = useStyles();
+  const {
+    reset,
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TransactionFormInput>();
 
-        let state = {
-            projectId:projectIdTrans,
-            amount: data.amount.toString()
-        }
-        
-        try{
-        const data = await postTransacData(state);
-        console.log(data);
-        transactionsList = [data, ...transactionsList];
-        
-        // listUpdated([state,...transitions])
-        // transactions.push(data)
-        // listUpdated(transactions)
-        // window.location.reload();
-        reset();
+  const onSubmit = async (data: TransactionFormInput) => {
+    //const{projectId, amount} = data
+
+    let state = {
+      projectId: projectIdTrans,
+      amount: data.amount.toString(),
+    };
+
+    try {
+      const data = await postTransacData(state);
+      console.log(data);
+      onTransactionCreated(data);
+
+      // listUpdated([state,...transitions])
+      // transactions.push(data)
+      // listUpdated(transactions)
+      // window.location.reload();
+      reset();
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
-  }
+  };
   return (
     <Paper>
-    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}
-        onReset={() => reset()} autoComplete = "off">
-    <Grid container>
-    <Grid item xs = {6} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
-    <Typography margin={"0.5em"} variant="h4">Fund a project</Typography>
-    </Grid>
-    </Grid>
-    <Grid container>
-        <Grid item xs = {4}>
+      <form
+        className={classes.root}
+        onSubmit={handleSubmit(onSubmit)}
+        onReset={() => reset()}
+        autoComplete="off"
+      >
+        <Grid container>
+          <Grid
+            item
+            xs={6}
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <Typography margin={"0.5em"} variant="h4">
+              Fund a project
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={4}>
             <TextField
-            {...register("amount", {min: 0.01,})} 
-            type = "number"
-            label = "Input funding amount"
-            defaultValue = {0}
-            error={errors.amount !== undefined}
+              {...register("amount", { min: 0.01 })}
+              type="number"
+              label="Input funding amount"
+              defaultValue={0}
+              error={errors.amount !== undefined}
             />
-            {errors.amount && ( 
-            <Typography variant ="body2" color ="red">Funding amount must be valid</Typography>)}
-            
-        </Grid>
-        <Grid item xs = {4} style={{ display: "flex", justifyContent: "flex-start", alignItems: "center"  }}>
+            {errors.amount && (
+              <Typography variant="body2" color="red">
+                Funding amount must be valid
+              </Typography>
+            )}
+          </Grid>
+          <Grid
+            item
+            xs={4}
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
             <Typography>(USD) Funding Amount</Typography>
+          </Grid>
         </Grid>
-    </Grid>
-    <Grid container>
-        <Grid item xs = {2}>
-            <Button variant="contained" 
-            color="primary"
-            type="reset">
-            <Typography variant="button">Reset</Typography>
+        <Grid container>
+          <Grid item xs={2}>
+            <Button variant="contained" color="primary" type="reset">
+              <Typography variant="button">Reset</Typography>
             </Button>
-        </Grid>
-        <Grid item xs = {2} >
-            <Button variant="contained" 
-            color="primary"
-            type="submit">
-            <Typography variant="button">Submit</Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Button variant="contained" color="primary" type="submit">
+              <Typography variant="button">Submit</Typography>
             </Button>
+          </Grid>
         </Grid>
-    </Grid>
-    </form>
+      </form>
     </Paper>
-    
-  )
+  );
 }
