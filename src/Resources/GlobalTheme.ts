71f4d07815c127585a3dc99b489bb7fa96caf564
Brown <brisybri@b0f1d8769f2a.ant.amazon.com>
2022-08-29 12:14:00 -0400

    Modifying global theme

--- src/Resources/GlobalTheme.ts
+++ src/Resources/GlobalTheme.ts
@@ -1,6 +1,5 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-
 export const theme = createTheme({
   palette: {
     primary: {
@@ -50,24 +49,3 @@ declare module '@mui/material/Button' {
     black: true;
   }
 }
-// export const theme = responsiveFontSizes(createTheme({
-//   palette: {
-//     primary: {
-//       main: '#335436',
-//     },
-//     secondary: {
-//       main: '#A6BBA7',
-//     },
-//   },
-//   typography: {
-//     fontFamily: 'Poppins',
-//   },
-//   components: {
-//     MuiTypography: {
-//       defaultProps: {
-//         fontFamily: 'sans-serif',
-//       }
-//     }
-//   }
-// })
-// );
